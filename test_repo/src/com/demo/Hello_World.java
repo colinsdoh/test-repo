ea913c0d75f71c1aa41d5aca81410140d3d368f5
colinsdoh <colinsdoh@nyongbelas-imac.lan1>
2020-04-12 21:55:31 -0400

    First commit

---
+++ test_repo/src/com/demo/Hello_World.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Hello_World {
+	public static void main(String args[]){
+	
+	int a = 2;
+	
+	int b = a - 4;
+
+	
+	int c = b + a - 20;
+	
+	System.out.println(c);
+
+}
+}
